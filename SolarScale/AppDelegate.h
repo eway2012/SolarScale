003a196306934e23da7d69e7d58b01c17db9d3ca
Sean Kladek <skladek@gmail.com>
2012-04-30 13:05:47 -0500

    Init Commit

---
+++ SolarScale/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SolarScale
+//
+//  Created by Sean Kladek on 4/30/12.
+//  Copyright (c) 2012 The Nerdery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SolarScale/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SolarScale
+//
+//  Created by Sean Kladek on 4/30/12.
+//  Copyright (c) 2012 The Nerdery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
